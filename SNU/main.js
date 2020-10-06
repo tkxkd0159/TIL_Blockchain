dddad93ff73fc57c6c3b525922d782f2c62f39ec
JAESEUNG LEE <41176085+tkxkd0159@users.noreply.github.com>
2020-10-07 01:57:24 +0900

    init blockchain JS project

---
+++ SNU/main.js
@@ -0,0 +1,15 @@
+// print "Hello!" to console
+console.log("Hello!");
+
+// print variable to console
+var one = 1;
+console.log("one = ", one);
+
+var two = 2;
+console.log("one plus two = ", one + two);
+
+var three = one + two;
+console.log("three = " + three);
+
+var string = "Hello BlockChain!";
+console.log(string);
---
+++ SNU/newBlock.js
@@ -0,0 +1,13 @@
+var Web3 = require('web3');
+
+var web3 = new Web3(new Web3.providers.WebsocketProvider('wss://mainnet.infura.io/ws/v3/d1d5cddfa64244f68b1c0359d22a5491'));
+
+web3.eth.subscribe('newBlockHeaders', (error, result) => {
+	if (!error) {
+		console.log('Check point', result);
+	} else {
+		console.log('error', error);
+	}
+}).on('data', function (transaction) {
+	console.log('block: ', transaction);
+})
