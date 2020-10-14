53cd48893452b3bb92a247f4f1e124a4e966bbdb
RLholic <tkxkd0159@gmail.com>
2020-10-14 14:56:26 +0900

    Subscribe ETH blocks thorough infura

--- SNU/newBlock.js
+++ SNU/newBlock.js
@@ -1,13 +1,20 @@
-var Web3 = require('web3');
+// Subscribe to newly created blocks
+var Web3 = require("web3");
 
-var web3 = new Web3(new Web3.providers.WebsocketProvider('wss://mainnet.infura.io/ws/v3/d1d5cddfa64244f68b1c0359d22a5491'));
+var web3 = new Web3(
+  new Web3.providers.WebsocketProvider(
+    "wss://mainnet.infura.io/ws/v3/d1d5cddfa64244f68b1c0359d22a5491"
+  )
+);
 
-web3.eth.subscribe('newBlockHeaders', (error, result) => {
-	if (!error) {
-		console.log('Check point', result);
-	} else {
-		console.log('error', error);
-	}
-}).on('data', function (transaction) {
-	console.log('block: ', transaction);
-})
+web3.eth
+  .subscribe("newBlockHeaders", (error, result) => {
+    if (!error) {
+      console.log("Check point", result);
+    } else {
+      console.log("error", error);
+    }
+  })
+  .on("data", function (transaction) {
+    console.log("block: ", transaction);
+  });
